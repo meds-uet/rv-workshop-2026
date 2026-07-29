7d73c9b21c5e3e40c3cead19b54269c27be1dca6
Muhammad-Faizan-Irfan <muhammadfaizan9421@gmail.com>
2026-07-29 17:04:16 +0500

    Implement the logic in registers file

--- project/rtl/reg_file.sv
+++ project/rtl/reg_file.sv
@@ -10,7 +10,7 @@
 module register_file (
     input  logic        clk,
     input  logic        we,
-    input logic reset,
+    input  logic        reset,
     input  logic [4:0]  ra1, ra2, wa,
     input  logic [31:0] wd,
     output logic [31:0] rd1, rd2
@@ -19,11 +19,30 @@ module register_file (
     logic [31:0] registers [0:31];
 
     // TODO: Initialize registers to zero using a for loop
+    always_comb begin
+        for (int i = 0; i < 32; i++)begin
+            registers[i] = 0;
+        end
+    end
+
     // Read port 1 (example implemented)
     assign rd1 = (ra1 == 5'b00000) ? 32'h0000_0000 : registers[ra1];
+    assign rd2 = (ra2 == 5'b00000) ? 32'h0000_0000 : registers[ra2];
 
     // TODO: Implement rd2 read port using same logic as rd1
     // TODO: Implement write logic (on clk posedge)
     // Only write if we == 1 and wa != x0
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            for (int i = 0; i < 32; i++) begin
+                registers[i] <= 0;
+            end
+        end
+        else if (we) begin
+            if (wa != 0) begin
+                registers[wa] <= wd;
+            end
+        end    
+    end
 
 endmodule
