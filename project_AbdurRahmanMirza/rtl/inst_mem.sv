177d040cd452086e1556af9942572d1cb5377a08
Abdur Rahman Mirza <a.rahmanmirza20@gmail.com>
2026-07-29 18:39:18 +0500

    feat: implement instruction memory module

--- project_AbdurRahmanMirza/rtl/inst_mem.sv
+++ project_AbdurRahmanMirza/rtl/inst_mem.sv
@@ -9,6 +9,7 @@
 
 module imem (
     input  logic [31:0] addr,
+    
     output logic [31:0] instruction
 );
 
@@ -17,19 +18,16 @@ module imem (
 
     initial begin
         // program instructions
-        mem[0] = 32'h0000_0093; // add  x1,  x0, 0
-        mem[1] = 32'h0000_0112; // addi x2, x0, 0
-        mem[2] = 32'h00A0_0193; // addi x3, x0, 10
-        mem[3] = 32'h0031_5463; // bge  x2, x3, 20
-        mem[4] = 32'h0020_80B3; // add  x1, x1, x2
-        mem[5] = 32'h0110_0113; // addi x2, x2, 1
- 
-        // fill the remaining memory with NOPs
-        for (i = 6; i < 1024; i = i + 1) begin
-            mem[i] = 32'h00000013; // NOP (addi x0, x0, 0)
-        end
-    end
+        mem[0] = 32'h00500093; // addi x1, x0, 5
+        mem[1] = 32'h00600113; // addi x2, x0, 6
+        mem[2] = 32'h002081B3; // add  x3, x1, x2
+        mem[3] = 32'h00000013; // nop
 
+        // fill remaining memory with NOPs
+        for (i = 4; i < 1024; i = i + 1)
+            mem[i] = 32'h00000013;
+    end
+    
     // word-aligned access
     assign instruction = mem[addr[31:2]];
 
