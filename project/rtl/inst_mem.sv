459556e9ac1484ecfa225004adf32c0f9caecd27
Husnain Jatoi <husnainjatoi716@gmail.com>
2026-07-29 23:16:46 +0500

    Add test instructions to instruction memory

--- project/rtl/inst_mem.sv
+++ project/rtl/inst_mem.sv
@@ -12,13 +12,19 @@ module imem (
 );
     logic [31:0] mem [0:1023]; // 4KB instruction memory
     initial begin
-        // Example instruction
+
         mem[0] = 32'h00500093; // addi x1, x0, 5
-        // TODO: Add more test instructions (e.g., addi, add, sub, etc.)
-        // TODO: Fill the remaining memory with NOPs (32'h00000013) using a for loop
-
-
+        mem[1] = 32'h00A00113; // addi x2, x0, 10
+        mem[2] = 32'h00208233; // add x4, x1, x2
+        mem[3] = 32'h401102B3; // sub x5, x2, x1
+        mem[4] = 32'h0020E4B3; // or x9, x1, x2
+        mem[5] = 32'h0020F533; // and x10, x1, x2
+        
+        // Fill remaining memory with NOPs
+        for(int i=6; i<1024; i++) begin
+            mem[i] = 32'h00000013;
         end
+    end
     // Word-aligned access
     assign instruction = mem[addr[31:2]];
 endmodule
