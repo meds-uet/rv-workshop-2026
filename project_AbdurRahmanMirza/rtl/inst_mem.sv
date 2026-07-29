d2b6a30664b2827e438529705d6609978c0dab44
Abdur Rahman Mirza <a.rahmanmirza20@gmail.com>
2026-07-29 14:06:50 +0500

    feat: implement instruction memory module

--- project_AbdurRahmanMirza/rtl/inst_mem.sv
+++ project_AbdurRahmanMirza/rtl/inst_mem.sv
@@ -6,19 +6,31 @@
 // =============================================================================
 // Single-Cycle RISC-V Processor - Instruction Memory (Workshop Skeleton Version)
 // =============================================================================
+
 module imem (
     input  logic [31:0] addr,
     output logic [31:0] instruction
 );
+
     logic [31:0] mem [0:1023]; // 4KB instruction memory
+    integer i;
+
     initial begin
-        // Example instruction
-        mem[0] = 32'h00500093; // addi x1, x0, 5
-        // TODO: Add more test instructions (e.g., addi, add, sub, etc.)
-        // TODO: Fill the remaining memory with NOPs (32'h00000013) using a for loop
-
-
+        // program instructions
+        mem[0] = 32'h0000_0093; // add  x1,  x0, 0
+        mem[1] = 32'h0000_0112; // addi x2, x0, 0
+        mem[2] = 32'h00A0_0193; // addi x3, x0, 10
+        mem[3] = 32'h0031_5463; // bge  x2, x3, 20
+        mem[4] = 32'h0020_80B3; // add  x1, x1, x2
+        mem[5] = 32'h0110_0113; // addi x2, x2, 1
+ 
+        // fill the remaining memory with NOPs
+        for (i = 6; i < 1024; i = i + 1) begin
+            mem[i] = 32'h00000013; // NOP (addi x0, x0, 0)
         end
-    // Word-aligned access
+    end
+
+    // word-aligned access
     assign instruction = mem[addr[31:2]];
+
 endmodule
