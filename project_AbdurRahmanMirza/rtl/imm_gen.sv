9efcc0da816639489d4cbca14871951df78688da
Abdur Rahman Mirza <a.rahmanmirza20@gmail.com>
2026-07-29 13:32:14 +0500

    refactor: update Imm_Gen module

--- project_AbdurRahmanMirza/rtl/imm_gen.sv
+++ project_AbdurRahmanMirza/rtl/imm_gen.sv
@@ -18,7 +18,7 @@ module immgen (
             3'b001: // S-type
                 imm_ext = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
             3'b010: // B-type
-                imm_ext = {{20{instruction[31]}}, instruction[7], Instr[30:25], instruction[11:8], 1'b0};
+                imm_ext = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
             3'b100: // U-type
                 imm_ext = {instruction[31:12], 12'b0};
             3'b011: // J-type
